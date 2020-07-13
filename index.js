a7756ca2eab92153f23df2a86488bd9e1f7e9b06
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-13 22:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-1 + 80 // 81
-60 - 40 // 20
-2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
-5.0 / 2.5 // 2
-
-
-
+function add(x,y) {
+  return x + y; 
+}
+function subtract(x,y) {
+  return x - y;
+}
+function multiply(x,y) {
+  return x * y;
+}
+function divide(x,y) {
+  return x / y;
+}
 /* 
 function makeInt(string) {
   return string.parseInt(string,10);
