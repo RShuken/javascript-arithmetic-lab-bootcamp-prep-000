5ac4711f4feeda73ed674c3f59be7536d7e6862c
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-13 22:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+
 function add(x,y) {
   return x + y; 
 }
+
 function subtract(x,y) {
   return x - y;
 }
+
 function multiply(x,y) {
   return x * y;
 }
+
 function divide(x,y) {
   return x / y;
+}
+
+function increment(n) {
+  return n++
+}
+
+function decrement(n) {
+  return n--
 }
 /* 
 function makeInt(string) {
